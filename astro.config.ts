9743256a9f638ec626fe21ae36c67c5cf89cee7f
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2026-06-14 12:15:06 +0000

    feat: use cdn base url to load static resources from

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+
+  build: {
+    assetsPrefix: 'https://cdn.jsdelivr.net/gh/e3stpavel/astro-images-cdn/dist',
+  },
+
   server: {
     host: true,
   },
