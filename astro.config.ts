b1f38180ffa885bb0ca9febf5db0c92bd0ffcc9d
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2026-06-18 12:45:35 +0000

    ci: configure deploy workflow

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   output: 'static',
 
+  site: 'https://e3stpavel.github.io',
+  base: '/astro-images-cdn',
+
   build: {
     assetsPrefix: 'https://cdn.jsdelivr.net/gh/e3stpavel/astro-images-cdn/dist',
   },
